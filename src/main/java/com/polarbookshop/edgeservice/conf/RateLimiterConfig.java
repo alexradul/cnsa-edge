21d8a63ced4a05b20a2596783c58d84bbbc445c2
Aleksandar Radulovic <aleksandar.radulovic@gmail.com>
2023-03-21 09:49:57 +0100

    Configuring Redis Rate Limiter

---
+++ src/main/java/com/polarbookshop/edgeservice/conf/RateLimiterConfig.java
@@ -0,0 +1,14 @@
+package com.polarbookshop.edgeservice.conf;
+
+import org.springframework.cloud.gateway.filter.ratelimit.KeyResolver;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import reactor.core.publisher.Mono;
+
+@Configuration
+public class RateLimiterConfig {
+    @Bean
+    KeyResolver keyResolver() {
+        return exchange -> Mono.just("anonymous");
+    }
+}
